aa841a2ab2eb96b2a793b7b29de9cffa69ae971e
Simone Leone <45096546+Esselle7@users.noreply.github.com>
2024-05-15 19:23:30 +0200

    test

--- example/test/recipe_list_controller_test.dart
+++ example/test/recipe_list_controller_test.dart
@@ -1,51 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
 import 'package:ultralytics_yolo_example/providers/recipes_list_controller.dart';
-import 'package:ultralytics_yolo_example/widgets/recipe_view.dart';
-
-class MockBuildContext extends Mock implements BuildContext {}
 
 void main() {
-  testWidgets('buildListRecipes builds the dialog with recipe items correctly', (WidgetTester tester) async {
-    // Preparazione dei dati di prova
+  testWidgets('Test buildListRecipes method', (WidgetTester tester) async {
     final jsonResponse = {
       'hits': [
-        {'recipe': {'label': 'Recipe 1', 'image': 'https://example.com/image1.jpg'}},
-        {'recipe': {'label': 'Recipe 2', 'image': 'https://example.com/image2.jpg'}},
-        {'recipe': {'label': 'Recipe 3', 'image': 'https://example.com/image3.jpg'}},
-        {'recipe': {'label': 'Recipe 4', 'image': 'https://example.com/image4.jpg'}},
-        {'recipe': {'label': 'Recipe 5', 'image': 'https://example.com/image5.jpg'}},
+        {
+          'recipe': {
+            'label': 'Test Recipe',
+            'image': 'https://example.com/image.jpg',
+            'totalNutrients': {
+              'CHOCDF': {'quantity': 10},
+              'PROCNT': {'quantity': 20},
+              'SUGAR': {'quantity': 5},
+              'FAT': {'quantity': 15},
+              'VITA_RAE': {'quantity': 100},
+              'VITC': {'quantity': 10},
+              'THIA': {'quantity': 5},
+              'NIA': {'quantity': 15},
+              'VITB6A': {'quantity': 20},
+            },
+            'totalWeight': 100,
+          },
+        },
       ],
     };
 
-    // Costruisci il widget di test
     await tester.pumpWidget(
       MaterialApp(
-        home: Builder(
-          builder: (context) {
-            ListRecipes.buildListRecipes(context, jsonResponse);
-            return Container(); // Un widget vuoto per evitare errori di rendering nel test
-          },
+        home: Scaffold(
+          body: Builder(
+            builder: (BuildContext context) {
+              return ElevatedButton(
+                onPressed: () {
+                  ListRecipes.buildListRecipes(context, jsonResponse);
+                },
+                child: Text('Show Recipes'),
+              );
+            },
+          ),
         ),
       ),
     );
 
-    // Attendi il rendering del dialog
-    await tester.pump();
+    await tester.tap(find.text('Show Recipes'));
+    await tester.pumpAndSettle();
 
-    // Controlla se il dialog è stato costruito correttamente
+    // Verifica che la finestra di dialogo sia stata visualizzata
     expect(find.byType(AlertDialog), findsOneWidget);
+    // Verifica che non ci siano widget di ricetta
+      // Trova il widget SingleChildScrollView
+  final scrollViewFinder = find.byType(SingleChildScrollView);
+
+  // Verifica che il widget SingleChildScrollView sia stato trovato
+  expect(scrollViewFinder, findsOneWidget);
+
+  // Verifica che il widget SingleChildScrollView sia vuoto
+  expect(find.descendant(of: scrollViewFinder, matching: find.byType(Column)), findsOneWidget);
+
+    // Verifica che il titolo della finestra di dialogo sia corretto
     expect(find.text('Delightful Selections'), findsOneWidget);
 
-    // Controlla se i widget di ricetta sono stati costruiti correttamente all'interno del dialog
-    expect(find.text('Recipe 1'), findsOneWidget);
-    expect(find.text('Recipe 2'), findsOneWidget);
-    expect(find.text('Recipe 3'), findsOneWidget);
-    expect(find.text('Recipe 4'), findsOneWidget);
-    expect(find.text('Recipe 5'), findsOneWidget);
+    // Tappa sul pulsante di chiusura
+    await tester.tap(find.text('Close'));
+    await tester.pumpAndSettle();
 
-    // Controlla se le immagini delle ricette sono state caricate correttamente
-    expect(find.byWidgetPredicate((widget) => widget is Image && widget.image is NetworkImage), findsNWidgets(5));
+  // Verifica che la finestra di dialogo sia stata chiusa
+    expect(find.byType(AlertDialog), findsNothing);
+
   });
 }
